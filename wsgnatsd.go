3c7425b630b3cebf535d695b5b56d6cee4d718c0
Dennis Mårtensson <dennis@greta.io>
2016-03-02 16:33:48 +0100

    have done some changes and added pub over ws

--- wsgnatsd.go
+++ wsgnatsd.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"runtime"
 	"strings"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/nats-io/nats"
@@ -17,16 +18,24 @@ import (
 // Version is the version number
 var Version string
 
+const (
+	writeWait      = 10 * time.Second
+	pongWait       = 60 * time.Second
+	pingPeriod     = (pongWait * 9) / 10
+	maxMessageSize = 1024 * 1024
+)
+
 var n *nats.Conn
 
-//var upgrader = websocket.Upgrader{} // use default options
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
+	ReadBufferSize:  maxMessageSize * 2,
+	WriteBufferSize: maxMessageSize * 2,
 }
 
-// Publish a message to a subject
+// Publish a message to a subject over http POST
 func post(subject string, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -43,20 +52,72 @@ func get(w http.ResponseWriter, r *http.Request) {
 		log.Println("Error upgrading to a websocket connection", err)
 		return
 	}
+
+	defer func() {
+		conn.Close()
+		n.Close()
+	}()
+
 	messageType, payload, err := conn.ReadMessage()
 	subject := string(payload)
 	if err != nil {
 		log.Println("Error reading a message", err)
 		return
 	}
+
 	// Subscribe to nats messages
 	log.Println("Subscribed to", subject)
 	n.Subscribe(subject, func(m *nats.Msg) {
+		conn.SetWriteDeadline(time.Now().Add(writeWait))
 		err = conn.WriteMessage(messageType, m.Data)
 		if err != nil {
 			log.Println("Error writing a message", err)
+			//SHOULD WE CLOSE THE CONN AND THE NATS CONNECTION
 		}
 	})
+
+	//Start ticker for sending ping
+	ticker := time.NewTicker(pingPeriod)
+	go func() {
+		defer func() {
+			ticker.Stop()
+		}()
+		for {
+			select {
+			case <-ticker.C:
+				err = conn.WriteMessage(websocket.PingMessage, []byte{})
+				if err != nil {
+					log.Println("Error writing ping", err)
+					break;
+				}
+			}
+		}
+	}()
+
+	// Start read loop so we can publish a message to a subject over websocket
+	conn.SetReadLimit(maxMessageSize)
+	conn.SetReadDeadline(time.Now().Add(pongWait))
+	conn.SetPongHandler(func(string) error { conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Printf("error: %v", err)
+			break
+		}
+
+		mes := string(message)
+		index := strings.Index(mes, " ")
+
+		if index > -1 {
+			subject := mes[0:index]
+			data := mes[index:len(mes)]
+
+			log.Println("Send to", subject)
+			n.Publish(subject, []byte(data))
+		}
+	}
+
 	runtime.Goexit()
 }
 
